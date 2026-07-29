2e37cb948fd9861e8679be915afffbc9162798ac
Kushal Bendale <bendalekushal@gmail.com>
2026-07-29 14:10:45 +0530

    jobpulse modified

--- src/jobpulse/config.py
+++ src/jobpulse/config.py
@@ -2,6 +2,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+print("__file__ =", __file__)
+print(type(__file__))
+print(type(Path(__file__)))
+print(Path(__file__))
+
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 print(f"BASE_DIR = {BASE_DIR}")
@@ -14,3 +19,4 @@ ENVIRONMENT = os.getenv("ENVIRONMENT")
 API_TIMEOUT = int(os.getenv("API_TIMEOUT", "30"))
 
 print(APP_NAME)
+print(BASE_DIR)
